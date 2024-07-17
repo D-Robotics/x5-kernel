19b6842c77d07e58cad4b5c4030d9c4b8186abb2
v-lingfeng.sun <v-lingfeng.sun@horizon.cc>
2024-07-17 10:34:33 +0800

    ASoC: archband: feat [X5-148] Update sample format support
    Signed-off-by: v-lingfeng.sun <v-lingfeng.sun@horizon.cc>
    Change-Id: I121ffc3c854e6fa860596d82939911c02bb41ef3

--- sound/soc/archband/archband-pdm.c
+++ sound/soc/archband/archband-pdm.c
@@ -25,7 +25,7 @@ static const struct snd_pcm_hardware dw_pcm_hardware = {
 		 SNDRV_PCM_RATE_48000 | SNDRV_PCM_RATE_44100,
 	.rate_min = 16000,
 	.rate_max = 48000,
-	.formats = SNDRV_PCM_FMTBIT_S24_LE | SNDRV_PCM_FMTBIT_S32_LE,
+	.formats = SNDRV_PCM_FMTBIT_S24_LE,
 	.channels_min = ARB_CHAN_MIN,
 	.channels_max = ARB_CHAN_MAX,
 	.buffer_bytes_max = BUFFER_BYTES_MAX,
@@ -145,7 +145,6 @@ static int arb_pdm_hw_params(struct snd_pcm_substream *substream,
 
 	switch (params_format(params)) {
 	case SNDRV_PCM_FORMAT_S24_LE:
-	case SNDRV_PCM_FORMAT_S32_LE:
 		config->data_width = 32;
 		break;
 	default:
@@ -558,7 +557,7 @@ static struct snd_soc_dai_driver arb_dai_driver = {
 	.ops = &arb_pdm_dai_ops,
 	.capture.channels_min = ARB_CHAN_MIN,
 	.capture.channels_max = ARB_CHAN_MAX,
-	.capture.formats = (SNDRV_PCM_FMTBIT_S24_LE | SNDRV_PCM_FMTBIT_S32_LE),
+	.capture.formats = (SNDRV_PCM_FMTBIT_S24_LE),
 	.capture.rates = SNDRV_PCM_RATE_16000 | SNDRV_PCM_RATE_32000 |
 			 SNDRV_PCM_RATE_48000 | SNDRV_PCM_RATE_44100,
 };
