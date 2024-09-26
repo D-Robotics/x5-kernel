65145e59c815fc269c74be0fd987a2e8b470d398
zhiwei01.li <zhiwei01.li@horizon.cc>
2024-09-26 15:37:55 +0800

    drivers: socinfo: feat [X5-789] Support obtaining more socinfo
    Summary:
    path: /sys/class/socinfo/
    -soc_gen
    -soc_name
    -hw_name
    -board_version
    -board_name
    -bootdevice_name
    -soc_uid
    -ddr_vendor
    -ddr_type
    -ddr_freq
    -ddr_size
    -board_id
    -bak_slot
    
    Signed-off-by: zhiwei01.li <zhiwei01.li@horizon.cc>
    Change-Id: I54038d86811384258afa56f41fb31b54686ae964

--- drivers/soc/hobot/socinfo/socinfo.c
+++ drivers/soc/hobot/socinfo/socinfo.c
@@ -39,20 +39,19 @@ struct _DDR_vendor_info {
 	char *vendor_name;
 };
 
-const char *soc_id;
-const char *bootmode;
-const char *socuid;
-const char *origin_board_id;
-const char *board_id;
+const char *soc_gen;
+const char *soc_name;
+const char *hw_name;
+const char *board_version;
+const char *board_name;
+const char *bootdevice_name;
+const char *soc_uid;
 const char *ddr_vendor;
 const char *ddr_type;
 const char *ddr_freq;
 const char *ddr_size;
-const char *ddr_part_num;
-const char *som_name;
-const char *base_board_name;
-const char *board_name;
-const char *chip_id;
+const char *board_id;
+
 static unsigned int secure_chip = 0;
 static void __iomem *sec_flag_addr;
 struct _reg_info g_bak_slot_info;
@@ -124,107 +123,78 @@ const static struct _DDR_vendor_info ddr_vendor_info[] = {
 		.vendor_name = "unkown",
 	},
 };
+
 const char *ddr_freq_array[] = {"unkown", "3200", "3733", "4266"};
 const char *ddr_type_array[] = {"unkown", "lpddr4", "lpddr4x"};
-const char *ddr_size_array[] = {"1G", "2G", "4G", "8G"};
+const char *ddr_size_array[] = {"1024MB", "2048MB", "4096MB", "8192MB"};
 
-EXPORT_SYMBOL_GPL(base_board_name);
-
-#if 0
-unsigned int x2_board_id[] = {
-	0x100, 0x101, 0x102, 0x103, 0x200, 0x201, 0x202, 0x203, 0x204, 0x205,
-	0x300, 0x301, 0x302, 0x303, 0x304, 0x400, 0x401, 0x104, 0x105, 0x106,
-	0x107,
-};
-
-struct hobot_board_info board_of_id[] = {
-	[0] = {X2_SVB, "X2-SVB"},
-	[1] = {X2_DEV, "X2-DEV"},
-	[2] = {X2SOM1V8, "X2SOM1V8"},
-	[3] = {X2SOM3V3, "X2SOM3V3"},
-	[4] = {J2_SVB, "J2-SVB"},
-	[5] = {J2SOM, "J2SOM"},
-	[6] = {J2_Mono, "J2-Mono"},
-	[7] = {J2_DEV, "J2-DEV"},
-	[8] = {J2_SK, "J2-SK"},
-	[9] = {J2_SAM, "J2-SAM"},
-	[10] = {J2_Quad, "J2-Quad*"},
-	[11] = {J2_QuadJ2A, "J2-QuadJ2A"},
-	[12] = {J2_QuadJ2B, "J2-QuadJ2B"},
-	[13] = {J2_QuadJ2C, "J2-QuadJ2C"},
-	[14] = {J2_QuadJ2D, "J2-QuadJ2D"},
-	[15] = {J2_mm, "J2-mm"},
-        [16] = {J2_mm_s202, "J2-mm-s202"},
-	[17] = {X2_SOMFULL, "X2-SOMFULL"},
-	[18] = {X2_96BOARD, "X2_96BOARD"},
-	[19] = {X2_DEV512M, "X2-DEV512M"},
-	[20] = {X2_XIAOMI, "X2-XIAOMI"},
-	[21] = {Unknown, "Unknown"},
-};
-
-static int parse_boardid(uint32_t board_id)
+ssize_t soc_gen_show(struct class *class,
+		     struct class_attribute *attr, char *buf)
 {
-	int i = 0;
-
-	for (i = 0; i < ARRAY_SIZE(x2_board_id); i++) {
-		if (board_id == x2_board_id[i])
-			break;
-	}
-
-	if(i > ARRAY_SIZE(x2_board_id))
-		i = ARRAY_SIZE(x2_board_id);
-	return i;
-}
-#endif
-
-ssize_t id_show(struct class *class,
-			struct class_attribute *attr, char *buf)
-{
-	if (board_id == NULL)
+	if (!buf || !soc_gen)
 		return 0;
-
-	snprintf(buf, BUF_LEN, "%s", board_id);
-	strcat(buf, "\n");
+	snprintf(buf, BUF_LEN, "%s\n", soc_gen);
 
 	return strlen(buf);
 }
 
-ssize_t origin_id_show(struct class *class,
-			struct class_attribute *attr, char *buf)
+ssize_t soc_name_show(struct class *class,
+		      struct class_attribute *attr, char *buf)
 {
-	if (origin_board_id == NULL)
+	if (!buf || !soc_name)
 		return 0;
-
-	snprintf(buf, BUF_LEN, "%s\n", origin_board_id);
+	snprintf(buf, BUF_LEN, "%s\n", soc_name);
 
 	return strlen(buf);
 }
 
-
-ssize_t name_show(struct class *class,
-			struct class_attribute *attr, char *buf)
+ssize_t hw_name_show(struct class *class,
+		     struct class_attribute *attr, char *buf)
 {
-	snprintf(buf, BUF_LEN, "%s-%s-%s-%s\n", base_board_name, ddr_vendor, ddr_size, ddr_freq);
+	if (!buf || !hw_name)
+		return 0;
+	snprintf(buf, BUF_LEN, "%s\n", hw_name);
 
 	return strlen(buf);
 }
 
-ssize_t boot_show(struct class *class,
+ssize_t board_version_show(struct class *class,
 			struct class_attribute *attr, char *buf)
 {
-	if (!buf || !bootmode)
+	if (!buf || !board_version)
 		return 0;
-	snprintf(buf, BUF_LEN, "%s\n", bootmode);
+	snprintf(buf, BUF_LEN, "%s\n", board_version);
 
 	return strlen(buf);
 }
 
-ssize_t socuid_show(struct class *class,
+ssize_t board_name_show(struct class *class,
 			struct class_attribute *attr, char *buf)
 {
-	if (!buf)
+	if (!buf || !board_name || !ddr_vendor || !ddr_size || !ddr_freq)
 		return 0;
-	snprintf(buf, BUF_LEN, "%s\n", socuid);
+	snprintf(buf, BUF_LEN, "%s-%s-%s-%s\n",
+			 board_name, ddr_vendor, ddr_size, ddr_freq);
+
+	return strlen(buf);
+}
+
+ssize_t bootdevice_name_show(struct class *class,
+			     struct class_attribute *attr, char *buf)
+{
+	if (!buf || !bootdevice_name)
+		return 0;
+	snprintf(buf, BUF_LEN, "%s\n", bootdevice_name);
+
+	return strlen(buf);
+}
+
+ssize_t soc_uid_show(struct class *class,
+		     struct class_attribute *attr, char *buf)
+{
+	if (!buf || !soc_uid)
+		return 0;
+	snprintf(buf, BUF_LEN, "%s\n", soc_uid);
 
 	return strlen(buf);
 }
@@ -232,17 +202,17 @@ ssize_t socuid_show(struct class *class,
 ssize_t ddr_vender_show(struct class *class,
 			struct class_attribute *attr, char *buf)
 {
-	if (!buf)
+	if (!buf || !ddr_vendor)
 		return 0;
 	snprintf(buf, BUF_LEN, "%s\n", ddr_vendor);
 
 	return strlen(buf);
 }
 
-ssize_t ddr_name_show(struct class *class,
+ssize_t ddr_type_show(struct class *class,
 			struct class_attribute *attr, char *buf)
 {
-	if (!buf)
+	if (!buf || !ddr_type)
 		return 0;
 	snprintf(buf, BUF_LEN, "%s\n", ddr_type);
 
@@ -252,7 +222,7 @@ ssize_t ddr_name_show(struct class *class,
 ssize_t ddr_freq_show(struct class *class,
 			struct class_attribute *attr, char *buf)
 {
-	if (!buf)
+	if (!buf || !ddr_freq)
 		return 0;
 	snprintf(buf, BUF_LEN, "%s\n", ddr_freq);
 
@@ -262,49 +232,20 @@ ssize_t ddr_freq_show(struct class *class,
 ssize_t ddr_size_show(struct class *class,
 			struct class_attribute *attr, char *buf)
 {
-	if (!buf)
+	if (!buf || !ddr_size)
 		return 0;
 	snprintf(buf, BUF_LEN, "%s\n", ddr_size);
 
 	return strlen(buf);
 }
 
-ssize_t ddr_part_num_show(struct class *class,
+ssize_t board_id_show(struct class *class,
 			struct class_attribute *attr, char *buf)
 {
-	if (!buf)
+	if (!buf || !board_id)
 		return 0;
-	snprintf(buf, BUF_LEN, "%s\n", ddr_part_num);
 
-	return strlen(buf);
-}
-
-ssize_t som_name_show(struct class *class,
-			struct class_attribute *attr, char *buf)
-{
-	if (!buf)
-		return 0;
-	snprintf(buf, BUF_LEN, "%s\n", som_name);
-
-	return strlen(buf);
-}
-
-ssize_t base_board_name_show(struct class *class,
-			struct class_attribute *attr, char *buf)
-{
-	if (!buf)
-		return 0;
-	snprintf(buf, BUF_LEN, "%s\n", base_board_name);
-
-	return strlen(buf);
-}
-
-ssize_t chip_id_show(struct class *class,
-			struct class_attribute *attr, char *buf)
-{
-	if (!buf)
-		return 0;
-	snprintf(buf, BUF_LEN, "0x%s\n", chip_id);
+	snprintf(buf, BUF_LEN, "%s\n", board_id);
 
 	return strlen(buf);
 }
@@ -312,7 +253,7 @@ ssize_t chip_id_show(struct class *class,
 ssize_t secure_chip_show(struct class *class,
 			struct class_attribute *attr, char *buf)
 {
-	if (!buf)
+	if (!buf || !secure_chip)
 		return 0;
 	snprintf(buf, BUF_LEN, "%d\n", secure_chip);
 
@@ -331,12 +272,6 @@ ssize_t bak_slot_show(struct class *class,
 	return strlen(buf);
 }
 
-ssize_t soc_store(struct class *class, struct class_attribute *attr,
-				const char *buf, size_t count)
-{
-	return count;
-}
-
 ssize_t bak_slot_store(struct class *class, struct class_attribute *attr,
 				const char *buf, size_t count)
 {
@@ -350,44 +285,41 @@ ssize_t bak_slot_store(struct class *class, struct class_attribute *attr,
 	return count;
 }
 
-static struct class_attribute name_attribute =
-	__ATTR(board_name, 0644, name_show, soc_store);
+static struct class_attribute soc_gen_attribute =
+	__ATTR(soc_gen, 0444, soc_gen_show, NULL);
 
-static struct class_attribute id_attribute =
-	__ATTR(board_id, 0644, id_show, soc_store);
+static struct class_attribute soc_name_attribute =
+	__ATTR(soc_name, 0444, soc_name_show, NULL);
 
-static struct class_attribute origin_id_attribute =
-	__ATTR(origin_board_id, 0644, origin_id_show, soc_store);
+static struct class_attribute hw_name_attribute =
+	__ATTR(hw_name, 0444, hw_name_show, NULL);
+
+static struct class_attribute board_version_attribute =
+	__ATTR(board_version, 0444, board_version_show, NULL);
+
+static struct class_attribute board_name_attribute =
+	__ATTR(board_name, 0444, board_name_show, NULL);
+
+static struct class_attribute bootdevice_name_attribute =
+	__ATTR(bootdevice_name, 0444, bootdevice_name_show, NULL);
+
+static struct class_attribute soc_uid_attribute =
+	__ATTR(soc_uid, 0444, soc_uid_show, NULL);
 
 static struct class_attribute ddr_vender_attribute =
-	__ATTR(ddr_vendor, 0644, ddr_vender_show, soc_store);
+	__ATTR(ddr_vendor, 0444, ddr_vender_show, NULL);
 
-static struct class_attribute ddr_name_attribute =
-	__ATTR(ddr_type, 0644, ddr_name_show, soc_store);
+static struct class_attribute ddr_type_attribute =
+	__ATTR(ddr_type, 0444, ddr_type_show, NULL);
 
 static struct class_attribute ddr_freq_attribute =
-	__ATTR(ddr_freq, 0644, ddr_freq_show, soc_store);
+	__ATTR(ddr_freq, 0444, ddr_freq_show, NULL);
 
 static struct class_attribute ddr_size_attribute =
-	__ATTR(ddr_size, 0644, ddr_size_show, soc_store);
+	__ATTR(ddr_size, 0444, ddr_size_show, NULL);
 
-static struct class_attribute ddr_part_num_attribute =
-	__ATTR(ddr_part_num, 0644, ddr_part_num_show, soc_store);
-
-static struct class_attribute som_name_attribute =
-	__ATTR(som_name, 0644, som_name_show, soc_store);
-
-static struct class_attribute base_board_name_attribute =
-	__ATTR(base_board_name, 0644, base_board_name_show, soc_store);
-
-static struct class_attribute boot_attribute =
-	__ATTR(boot_mode, 0644, boot_show, soc_store);
-
-static struct class_attribute socuid_attribute =
-	__ATTR(soc_uid, 0644, socuid_show, soc_store);
-
-static struct class_attribute chip_id_attribute =
-	__ATTR(chip_id, 0644, chip_id_show, soc_store);
+static struct class_attribute board_id_attribute =
+	__ATTR(board_id, 0444, board_id_show, NULL);
 
 static struct class_attribute secure_chip_attribute =
 	__ATTR(secure_chip, 0444, secure_chip_show, NULL);
@@ -396,19 +328,18 @@ static struct class_attribute bak_slot_attribute =
 	__ATTR(bak_slot, 0644, bak_slot_show, bak_slot_store);
 
 static struct attribute *socinfo_attributes[] = {
-	&name_attribute.attr,
-	&id_attribute.attr,
-	&origin_id_attribute.attr,
+	&soc_gen_attribute.attr,
+	&soc_name_attribute.attr,
+	&hw_name_attribute.attr,
+	&board_version_attribute.attr,
+	&board_name_attribute.attr,
+	&bootdevice_name_attribute.attr,
+	&soc_uid_attribute.attr,
 	&ddr_vender_attribute.attr,
-	&ddr_name_attribute.attr,
+	&ddr_type_attribute.attr,
 	&ddr_freq_attribute.attr,
 	&ddr_size_attribute.attr,
-	&ddr_part_num_attribute.attr,
-	&som_name_attribute.attr,
-	&base_board_name_attribute.attr,
-	&boot_attribute.attr,
-	&socuid_attribute.attr,
-	&chip_id_attribute.attr,
+	&board_id_attribute.attr,
 	&secure_chip_attribute.attr,
 	&bak_slot_attribute.attr,
 	NULL
@@ -424,16 +355,31 @@ static const struct attribute_group *socinfo_attr_group[] = {
 };
 
 static struct class socinfo_class = {
-	.name = "socinfo",
+	.name = SOCINFO_NAME,
 	.class_groups = socinfo_attr_group,
 };
 
 /* Match table for of_platform binding */
 static const struct of_device_id socinfo_of_match[] = {
-	{ .compatible = "hobot,x5-socinfo", },
+	{ .compatible = "hobot,boardinfo", },
 	{}
 };
+
 MODULE_DEVICE_TABLE(of, socinfo_of_match);
+
+static int read_from_property(struct platform_device *pdev,
+			      const char *propname, const char **buf)
+{
+	int ret = 0;
+
+	ret = of_property_read_string(pdev->dev.of_node, propname,
+				      buf);
+	if (ret != 0) {
+		pr_err("of_property_read_string %s error\n", propname);
+	}
+
+	return ret;
+}
 
 static int socinfo_probe(struct platform_device *pdev)
 {
@@ -444,58 +390,15 @@ static int socinfo_probe(struct platform_device *pdev)
 
 	dev_info(&pdev->dev, "Start socinfo probe.\n");
 
-	ret = of_property_read_string(pdev->dev.of_node, "board_name",
-		&board_name);
-	if (ret != 0) {
-		pr_err("of_property_read_string error\n");
-		return ret;
-	}
-
-	ret = of_property_read_string(pdev->dev.of_node, "board_id",
-		&board_id);
-	if (ret != 0) {
-		pr_err("of_property_read_string error\n");
-		return ret;
-	}
-
-	ret = of_property_read_string(pdev->dev.of_node, "origin_board_id",
-		&origin_board_id);
-	if (ret != 0) {
-		pr_err("of_property_read_string error\n");
-		return ret;
-	}
-
-	ret = of_property_read_string(pdev->dev.of_node, "som_name",
-		&som_name);
-	if (ret != 0) {
-		pr_err("of_property_read_string error\n");
-		return ret;
-	}
-
-	ret = of_property_read_string(pdev->dev.of_node, "base_board_name",
-		&base_board_name);
-	if (ret != 0) {
-		pr_err("of_property_read_string error\n");
-		return ret;
-	}
-
-	ret = of_property_read_string(pdev->dev.of_node, "boot_mode",
-		&bootmode);
-	if (ret != 0) {
-		pr_err("of_property_read_string error\n");
-		return ret;
-	}
-
-	ret = of_property_read_string(pdev->dev.of_node, "socuid", &socuid);
-	if (ret != 0) {
-		pr_err("of_property_read_string error\n");
-		return ret;
-	}
-
-	ret = of_property_read_string(pdev->dev.of_node, "chip_id", &chip_id);
-	if (ret != 0) {
-		pr_err("of_property_read_string error\n");
-		return ret;
+	if (read_from_property(pdev, "soc_gen", &soc_gen) ||
+		read_from_property(pdev, "soc_name", &soc_name) ||
+		read_from_property(pdev, "hw_name", &hw_name) ||
+		read_from_property(pdev, "board_version", &board_version) ||
+		read_from_property(pdev, "board_name", &board_name) ||
+		read_from_property(pdev, "bootdevice_name", &bootdevice_name) ||
+		read_from_property(pdev, "soc_uid", &soc_uid) ||
+		read_from_property(pdev, "board_id", &board_id)) {
+		return -1;
 	}
 
 	ret = of_property_read_u32_array(pdev->dev.of_node,
