1ac39282afee9d557fe514a15c72a01b138e1594
Hualun Dong <hualun.dong@horizon.cc>
2024-09-26 15:14:23 +0800

    feat(qos): [X5-1935] Show/store qos advanced\mode\bandwidth\saturation\extcontrol value from user
    Add functions to show/store qos advanced\mode\bandwidth\saturation\extcontrol value from user for debugging
    convenient and refine cpu_qos to cpu_ace_qos and cpu_ace_qos because of
    different Bandwidth and priority.
    Modify ISP_qos name to avoid ambiguity.
    
    Signed-off-by: Hualun Dong <hualun.dong@horizon.cc>
    Change-Id: I0a58e738c3b18acd3f60db65d1c592cf671ca06c

--- drivers/soc/hobot/ddr_monitor/horizon_noc_qos.c
+++ drivers/soc/hobot/ddr_monitor/horizon_noc_qos.c
@@ -21,9 +21,14 @@
 #define NOC_QOS_PRIORITY_OFFSET (0x08)
 
 #define NOC_QOS_MODE_PRIORITY_OFFSET (0x0c - 0x08)
+#define NOC_QOS_BW_PRIORITY_OFFSET (0x10 - 0x08)
+#define NOC_QOS_SATURATION_PRIORITY_OFFSET (0x14 - 0x08)
+#define NOC_QOS_EXTCONTROL_PRIORITY_OFFSET (0x18 - 0x08)
 
 #define MAXPORTSNUM 5
 #define MAXCLOCKSNUM 10
+#define BW_REGISTER_MAX 4095
+#define SATURATION_REGISTER_MAX 1023
 
 struct noc_qos{
 	struct platform_device *pdev;
@@ -32,6 +37,10 @@ struct noc_qos{
 	int rvalue;
 	int wvalue;
 	int mode;
+	bool advanced;
+	unsigned int bandwidth;
+	unsigned int saturation;
+	int extcontrol;
 	struct device_link * link;
 	int ports_num;
 	int clocks_num;
@@ -126,7 +135,10 @@ static ssize_t priority_write_ctl_store(struct device *dev, struct device_attrib
 	}
 
 	nocqos->wvalue = write_ctl_value;
-	nocqos->mode = 0;
+	if (!nocqos->advanced)
+	{
+		nocqos->mode = 0;
+	}
 
 	priority = readl(nocqos->base + NOC_QOS_PRIORITY_OFFSET);
 	priority &= ~0x07;
@@ -227,7 +239,10 @@ static ssize_t priority_read_ctl_store(struct device *dev, struct device_attribu
 	}
 
 	nocqos->rvalue = read_ctl_value;
-	nocqos->mode = 0;
+	if (!nocqos->advanced)
+	{
+		nocqos->mode = 0;
+	}
 
 	priority = readl(nocqos->base + NOC_QOS_PRIORITY_OFFSET);
 	priority &= ~(0x07 << 8);
@@ -266,9 +281,367 @@ static struct attribute_group priority_read_attr_group = {
 	.attrs = priority_read_qctrl_attrs,
 };
 
+static ssize_t mode_ctl_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	unsigned int mode = 0, ret = 0;
+	struct noc_qos* nocqos = dev_get_drvdata(dev);
+
+	ret = pm_runtime_resume_and_get(dev);
+	if (ret) {
+		dev_err(dev, "failed to resume qos %d\n", ret);
+		return -EINVAL;
+	}
+
+	if(qos_clk_enable(nocqos)){
+		dev_err(dev, "Failed to clk enable\n");
+		return -1;
+	}
+
+	mode = readl(nocqos->base + NOC_QOS_PRIORITY_OFFSET + NOC_QOS_MODE_PRIORITY_OFFSET);
+
+	if(qos_clk_disable(nocqos)){
+		dev_err(dev, "Failed to clk disable\n");
+		return -1;
+	}
+
+	ret = pm_runtime_put_sync_suspend(dev);
+	if (ret) {
+		dev_err(dev, "Failed to suspend %d\n", ret);
+		return -EINVAL;
+	}
+
+	return sprintf(buf, "qos_mode : %d\n", mode);
+}
+
+static ssize_t mode_ctl_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
+{
+	int ret;
+	unsigned int i;
+	unsigned int mode_ctl_value = 0;
+	struct noc_qos* nocqos = dev_get_drvdata(dev);
+	int ports_num = nocqos->ports_num;
+
+	ret = sscanf(buf, "%du", &mode_ctl_value);
+	if (mode_ctl_value > 3) {
+		dev_err(dev, "set value %d error,you should set 0~3\n", mode_ctl_value);
+		return count;
+	}
+
+	ret = pm_runtime_resume_and_get(dev);
+	if (ret) {
+		dev_err(dev, "failed to resume qos %d\n", ret);
+		return -EINVAL;
+	}
+
+	if(qos_clk_enable(nocqos)){
+		dev_err(dev, "Failed to clk enable\n");
+		return -1;
+	}
+
+	nocqos->mode = mode_ctl_value;
+
+	for(i = 0; i < ports_num; i++)
+	{
+		writel(mode_ctl_value, nocqos->base + nocqos->ports_off[i] + NOC_QOS_MODE_PRIORITY_OFFSET);
+	}
+
+	if(qos_clk_disable(nocqos)){
+		dev_err(dev, "Failed to clk disable\n");
+		return -1;
+	}
+
+	ret = pm_runtime_put_sync_suspend(dev);
+	if (ret) {
+		dev_err(dev, "Failed to suspend %d\n", ret);
+		return -EINVAL;
+	}
+
+	return count;
+}
+
+static ssize_t bandwidth_ctl_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	unsigned int bandwidth = 0, ret = 0;
+	struct noc_qos* nocqos = dev_get_drvdata(dev);
+
+	ret = pm_runtime_resume_and_get(dev);
+	if (ret) {
+		dev_err(dev, "failed to resume qos %d\n", ret);
+		return -EINVAL;
+	}
+
+	if(qos_clk_enable(nocqos)){
+		dev_err(dev, "Failed to clk enable\n");
+		return -1;
+	}
+
+	bandwidth = readl(nocqos->base + NOC_QOS_PRIORITY_OFFSET + NOC_QOS_BW_PRIORITY_OFFSET);
+
+	if(qos_clk_disable(nocqos)){
+		dev_err(dev, "Failed to clk disable\n");
+		return -1;
+	}
+
+	ret = pm_runtime_put_sync_suspend(dev);
+	if (ret) {
+		dev_err(dev, "Failed to suspend %d\n", ret);
+		return -EINVAL;
+	}
+
+	return sprintf(buf, "qos_bandwidth : %d\n", bandwidth);
+}
+
+static ssize_t bandwidth_ctl_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
+{
+	int ret;
+	unsigned int i;
+	unsigned int bandwidth_ctl_value = 0;
+	struct noc_qos* nocqos = dev_get_drvdata(dev);
+	int ports_num = nocqos->ports_num;
+
+	ret = sscanf(buf, "%du", &bandwidth_ctl_value);
+	if (bandwidth_ctl_value > BW_REGISTER_MAX) {
+		dev_err(dev, "set value error, bandwidth_ctl_value exceeds the register range > %d\n", BW_REGISTER_MAX);
+		return count;
+	}
+
+	ret = pm_runtime_resume_and_get(dev);
+	if (ret) {
+		dev_err(dev, "failed to resume qos %d\n", ret);
+		return -EINVAL;
+	}
+
+	if(qos_clk_enable(nocqos)){
+		dev_err(dev, "Failed to clk enable\n");
+		return -1;
+	}
+
+	nocqos->bandwidth = bandwidth_ctl_value;
+
+	for(i = 0; i < ports_num; i++)
+	{
+		writel(bandwidth_ctl_value, nocqos->base + nocqos->ports_off[i] + NOC_QOS_BW_PRIORITY_OFFSET);
+	}
+
+	if(qos_clk_disable(nocqos)){
+		dev_err(dev, "Failed to clk disable\n");
+		return -1;
+	}
+
+	ret = pm_runtime_put_sync_suspend(dev);
+	if (ret) {
+		dev_err(dev, "Failed to suspend %d\n", ret);
+		return -EINVAL;
+	}
+
+	return count;
+}
+
+static ssize_t extcontrol_ctl_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	unsigned int extcontrol = 0, ret = 0;
+	struct noc_qos* nocqos = dev_get_drvdata(dev);
+
+	ret = pm_runtime_resume_and_get(dev);
+	if (ret) {
+		dev_err(dev, "failed to resume qos %d\n", ret);
+		return -EINVAL;
+	}
+
+	if(qos_clk_enable(nocqos)){
+		dev_err(dev, "Failed to clk enable\n");
+		return -1;
+	}
+
+	extcontrol = readl(nocqos->base + NOC_QOS_PRIORITY_OFFSET + NOC_QOS_EXTCONTROL_PRIORITY_OFFSET);
+
+	if(qos_clk_disable(nocqos)){
+		dev_err(dev, "Failed to clk disable\n");
+		return -1;
+	}
+
+	ret = pm_runtime_put_sync_suspend(dev);
+	if (ret) {
+		dev_err(dev, "Failed to suspend %d\n", ret);
+		return -EINVAL;
+	}
+
+	return sprintf(buf, "qos_extcontrol : %d\n", extcontrol);
+}
+
+static ssize_t extcontrol_ctl_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
+{
+	int ret;
+	unsigned int i;
+	unsigned int extcontrol_ctl_value = 0;
+	struct noc_qos* nocqos = dev_get_drvdata(dev);
+	int ports_num = nocqos->ports_num;
+
+	sscanf(buf, "%du", &extcontrol_ctl_value);
+
+	ret = pm_runtime_resume_and_get(dev);
+	if (ret) {
+		dev_err(dev, "failed to resume qos %d\n", ret);
+		return -EINVAL;
+	}
+
+	if(qos_clk_enable(nocqos)){
+		dev_err(dev, "Failed to clk enable\n");
+		return -1;
+	}
+
+	nocqos->extcontrol = extcontrol_ctl_value;
+
+	for(i = 0; i < ports_num; i++)
+	{
+		writel(extcontrol_ctl_value, nocqos->base + nocqos->ports_off[i] + NOC_QOS_EXTCONTROL_PRIORITY_OFFSET);
+	}
+
+	if(qos_clk_disable(nocqos)){
+		dev_err(dev, "Failed to clk disable\n");
+		return -1;
+	}
+
+	ret = pm_runtime_put_sync_suspend(dev);
+	if (ret) {
+		dev_err(dev, "Failed to suspend %d\n", ret);
+		return -EINVAL;
+	}
+
+	return count;
+}
+
+static ssize_t saturation_ctl_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	unsigned int saturation = 0, ret = 0;
+	struct noc_qos* nocqos = dev_get_drvdata(dev);
+
+	ret = pm_runtime_resume_and_get(dev);
+	if (ret) {
+		dev_err(dev, "failed to resume qos %d\n", ret);
+		return -EINVAL;
+	}
+
+	if(qos_clk_enable(nocqos)){
+		dev_err(dev, "Failed to clk enable\n");
+		return -1;
+	}
+
+	saturation = readl(nocqos->base + NOC_QOS_PRIORITY_OFFSET + NOC_QOS_SATURATION_PRIORITY_OFFSET);
+
+	if(qos_clk_disable(nocqos)){
+		dev_err(dev, "Failed to clk disable\n");
+		return -1;
+	}
+
+	ret = pm_runtime_put_sync_suspend(dev);
+	if (ret) {
+		dev_err(dev, "Failed to suspend %d\n", ret);
+		return -EINVAL;
+	}
+
+	return sprintf(buf, "qos_saturation : %d\n", saturation);
+}
+
+static ssize_t saturation_ctl_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
+{
+	int ret;
+	unsigned int i;
+	unsigned int saturation_ctl_value = 0;
+	struct noc_qos* nocqos = dev_get_drvdata(dev);
+	int ports_num = nocqos->ports_num;
+
+	ret = sscanf(buf, "%du", &saturation_ctl_value);
+	if (saturation_ctl_value > SATURATION_REGISTER_MAX) {
+		dev_err(dev, "set value error, saturation_ctl_value exceeds the register range > %d\n", SATURATION_REGISTER_MAX);
+		return count;
+	}
+
+	ret = pm_runtime_resume_and_get(dev);
+	if (ret) {
+		dev_err(dev, "failed to resume qos %d\n", ret);
+		return -EINVAL;
+	}
+
+	if(qos_clk_enable(nocqos)){
+		dev_err(dev, "Failed to clk enable\n");
+		return -1;
+	}
+
+	nocqos->saturation = saturation_ctl_value;
+
+	for(i = 0; i < ports_num; i++)
+	{
+		writel(saturation_ctl_value, nocqos->base + nocqos->ports_off[i] + NOC_QOS_SATURATION_PRIORITY_OFFSET);
+	}
+
+	if(qos_clk_disable(nocqos)){
+		dev_err(dev, "Failed to clk disable\n");
+		return -1;
+	}
+
+	ret = pm_runtime_put_sync_suspend(dev);
+	if (ret) {
+		dev_err(dev, "Failed to suspend %d\n", ret);
+		return -EINVAL;
+	}
+
+	return count;
+}
+
+static ssize_t advancement_ctl_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	struct noc_qos* nocqos = dev_get_drvdata(dev);
+
+	return sprintf(buf, "qos_advanced : %d\n", nocqos->advanced);
+}
+
+static ssize_t advancement_ctl_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
+{
+	int ret;
+	unsigned int advancement_ctl_value = 0;
+	struct noc_qos* nocqos = dev_get_drvdata(dev);
+
+	ret = sscanf(buf, "%du", &advancement_ctl_value);
+	if ((advancement_ctl_value != 0) && (advancement_ctl_value != 1)) {
+		dev_err(dev, "set advanced value error, advancement_ctl_value is 0 or 1\n");
+		return count;
+	}
+
+	nocqos->advanced = advancement_ctl_value;
+
+	return count;
+}
+
+static struct device_attribute mode_ctl = __ATTR(mode, 0600,
+						   mode_ctl_show, mode_ctl_store);
+static struct device_attribute bandwidth_ctl = __ATTR(bandwidth, 0600,
+						   bandwidth_ctl_show, bandwidth_ctl_store);
+static struct device_attribute saturation_ctl = __ATTR(saturation, 0600,
+						   saturation_ctl_show, saturation_ctl_store);
+static struct device_attribute extcontrol_ctl = __ATTR(extcontrol, 0600,
+						   extcontrol_ctl_show, extcontrol_ctl_store);
+static struct device_attribute advancement_ctl = __ATTR(advanced, 0600,
+						   advancement_ctl_show, advancement_ctl_store);
+
+static struct attribute *mode_qctrl_attrs[] = {
+	&mode_ctl.attr,
+	&bandwidth_ctl.attr,
+	&saturation_ctl.attr,
+	&extcontrol_ctl.attr,
+	&advancement_ctl.attr,
+	NULL,
+};
+
+static struct attribute_group mode_attr_group = {
+	.name = "mode_qos_ctrl",
+	.attrs = mode_qctrl_attrs,
+};
+
 static const struct attribute_group *noc_qos_attr_groups[] = {
 	&priority_write_attr_group,
 	&priority_read_attr_group,
+	&mode_attr_group,
 	NULL,
 };
 
@@ -279,6 +652,9 @@ void noc_qos_restore(struct device *dev)
 	unsigned int mode = nocqos->mode;
 	unsigned int read_ctl_value = nocqos->rvalue;
 	unsigned int write_ctl_value = nocqos->wvalue;
+	unsigned int bandwidth = nocqos->bandwidth;
+	unsigned int saturation = nocqos->saturation;
+	int extcontrol = nocqos->extcontrol;
 	int ports_num = nocqos->ports_num;
 
 	priority |= write_ctl_value;
@@ -293,6 +669,9 @@ void noc_qos_restore(struct device *dev)
 	{
 		writel(mode, nocqos->base + nocqos->ports_off[i] + NOC_QOS_MODE_PRIORITY_OFFSET);
 		writel(priority, nocqos->base + nocqos->ports_off[i]);
+		writel(bandwidth, nocqos->base + nocqos->ports_off[i] + NOC_QOS_BW_PRIORITY_OFFSET);
+		writel(saturation, nocqos->base + nocqos->ports_off[i] + NOC_QOS_SATURATION_PRIORITY_OFFSET);
+		writel(extcontrol, nocqos->base + nocqos->ports_off[i] + NOC_QOS_EXTCONTROL_PRIORITY_OFFSET);
 	}
 
 	if(qos_clk_disable(nocqos)){
@@ -442,6 +821,9 @@ static int noc_qos_probe(struct platform_device *pdev)
 
 	p_value = readl(nocqos->base + NOC_QOS_PRIORITY_OFFSET);
 	m_value = readl(nocqos->base + NOC_QOS_PRIORITY_OFFSET + NOC_QOS_MODE_PRIORITY_OFFSET);
+	nocqos->bandwidth = readl(nocqos->base + NOC_QOS_PRIORITY_OFFSET + NOC_QOS_BW_PRIORITY_OFFSET);
+	nocqos->saturation = readl(nocqos->base + NOC_QOS_PRIORITY_OFFSET + NOC_QOS_SATURATION_PRIORITY_OFFSET);
+	nocqos->extcontrol = readl(nocqos->base + NOC_QOS_PRIORITY_OFFSET + NOC_QOS_EXTCONTROL_PRIORITY_OFFSET);
 	nocqos->rvalue = (p_value >> 8) & 0x07;
 	nocqos->wvalue = p_value & 0x07;
 	nocqos->mode = m_value;
