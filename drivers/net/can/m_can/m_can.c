a6aa150a15b5e148241ec03b639f8249b3447c7a
xin03.liu <xin03.liu@d-robotics.cc>
2025-04-24 16:30:00 +0800

    fix(can)[RDK-587]: Init the tcan4550, that it may not be able to read the m_can-vversion, but it can work
    Signed-off-by: xin03.liu <xin03.liu@d-robotics.cc>

--- drivers/net/can/m_can/m_can.c
+++ drivers/net/can/m_can/m_can.c
@@ -1475,9 +1475,10 @@ static int m_can_dev_setup(struct m_can_classdev *cdev)
 	m_can_version = m_can_check_core_release(cdev);
 	/* return if unsupported version */
 	if (!m_can_version) {
-		dev_err(cdev->dev, "Unsupported version number: %2d",
+		dev_info(cdev->dev, "Unsupported version number: %2d",
 			m_can_version);
-		return -EINVAL;
+		m_can_version = 32;
+		//return -EINVAL;
 	}
 
 	if (!cdev->is_peripheral)
